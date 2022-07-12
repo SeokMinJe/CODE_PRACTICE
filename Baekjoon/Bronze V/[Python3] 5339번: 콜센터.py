467dde370a199b3dc9445a2aba693c4d315443fd
SeokMinJe <76511709+SeokMinJe@users.noreply.github.com>
2022-07-12 14:29:14 +0900

    Create [Python3] 5339번: 콜센터.py

---
+++ Baekjoon/Bronze V/[Python3] 5339번: 콜센터.py
@@ -0,0 +1,11 @@
+# Solution
+print('     /~\\')
+print('    ( oo|')
+print('    _\=/_')
+print('   /  _  \\')
+print('  //|/.\|\\\\')
+print(' ||  \ /  ||')
+print('============')
+print('|          |')
+print('|          |')
+print('|          |')
